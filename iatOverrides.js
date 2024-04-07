28b090383e813b6d70c3927d7d60eb33360b1bb5
Sam Fenwick <samfenwick@live.com>
2024-04-07 20:58:40 +0100

    updated

---
+++ iatOverrides.js
@@ -0,0 +1,55 @@
+define(['pipAPI', 'https://samfenwick.github.io/test-iat-delete-me/iat.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		isTouch: navigator.maxTouchPoints > 0 ? true : false,
+		category1 : {
+			name : 'Fat people', //Will appear in the data.
+			title : {
+				media : {word : 'Fat people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'fatman1.jpg'}, 
+    			{image : 'fatman3.jpg'}, 
+    			{image : 'fatman4.jpg'}, 
+    			{image : 'fatman5.jpg'}, 
+    			{image : 'fatman6.jpg'}, 
+    			{image : 'fatwoman1.jpg'}, 
+    			{image : 'fatwoman2.jpg'}, 
+    			{image : 'fatwoman3.jpg'}, 
+    			{image : 'fatwoman4.jpg'}, 
+    			{image : 'fatwoman6.jpg'}, 
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+		category2 :	{
+			name : 'Thin people', //Will appear in the data.
+			title : {
+				media : {word : 'Thin people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+     		    {image : 'thinman1.jpg'}, 
+    			{image : 'thinman2.jpg'}, 
+    			{image : 'thinman3.jpg'}, 
+    			{image : 'thinman5.jpg'}, 
+    			{image : 'thinman6.jpg'}, 
+    			{image : 'thinwoman1.jpg'}, 
+    			{image : 'thinwoman2.jpg'}, 
+    			{image : 'thinwoman3.jpg'}, 
+    			{image : 'thinwoman5.jpg'}, 
+				{ image: 'thinwoman6.jpg' },
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+		} 
+	});
+});
